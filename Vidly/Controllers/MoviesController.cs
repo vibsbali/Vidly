a268d9c5c337c8ee8954db79e120a4384c2b478f
Vaibhav Bali <vibsbali@gmail.com>
2016-08-14 18:26:40 +1000

    Created Movies controller and Random View

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Random()
+        {
+            var movie = new Movie
+            {
+                Id = 1,
+                Name = "Shrek!"
+            };
+
+            return View(movie);
+        }
+    }
+}
