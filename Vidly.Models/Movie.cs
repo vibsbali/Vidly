a7182e357a674b9bebeb251dad5715af76869932
Vaibhav Bali <vibsbali@gmail.com>
2016-09-01 14:58:26 +1000

    Added new class Genre

--- Vidly.Models/Movie.cs
+++ Vidly.Models/Movie.cs
@@ -1,8 +1,28 @@
-﻿namespace Vidly.Models
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Vidly.Models
 {
     public class Movie
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
+        public DateTime ReleaseDate { get; set; }
+        [Required]
+        public DateTime DateAdded { get; set; }
+        [Required]
+        public int StockLevel { get; set; }
+
+        [Required]
+        public Genre Genre { get; set; }
+        public int GenreId { get; set; }
+
+        //Adding constructor so that the date is automatically populated
+        public Movie()
+        {
+            DateAdded = DateTime.Now;
+        }
     }
 }
