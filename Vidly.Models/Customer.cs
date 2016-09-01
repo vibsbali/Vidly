9f61274158fafbba94e1b5aae4ceed2a7f642d68
Vaibhav Bali <vibsbali@gmail.com>
2016-09-01 13:52:37 +1000

    Added MembershipType

--- Vidly.Models/Customer.cs
+++ Vidly.Models/Customer.cs
@@ -5,5 +5,8 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
+
+        public MembershipType MembershipType { get; set; }
+        public int MembershipTypeId { get; set; }
     }
 }
---
+++ Vidly.Models/MembershipType.cs
@@ -0,0 +1,10 @@
+﻿namespace Vidly.Models
+{
+    public class MembershipType
+    {
+        public int Id { get; set; }
+        public short SignUpFee { get; set; }
+        public short DurationInMonths { get; set; }
+        public double DiscountRate { get; set; }
+    }
+}
