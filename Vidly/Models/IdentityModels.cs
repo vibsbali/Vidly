8caa936d53a567492a9f92b526fba2082823a727
Vaibhav Bali <vibsbali@gmail.com>
2016-09-01 13:45:23 +1000

    Created database using Update-Database

--- Vidly/Models/IdentityModels.cs
+++ Vidly/Models/IdentityModels.cs
@@ -1,8 +1,8 @@
-﻿using System.Data.Entity;
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
 using System.Security.Claims;
 using System.Threading.Tasks;
-using Microsoft.AspNet.Identity;
-using Microsoft.AspNet.Identity.EntityFramework;
 
 namespace Vidly.Models
 {
@@ -24,6 +24,8 @@ namespace Vidly.Models
             : base("DefaultConnection", throwIfV1Schema: false)
         {
         }
+
+        public DbSet<Customer> Customers { get; set; }
 
         public static ApplicationDbContext Create()
         {
