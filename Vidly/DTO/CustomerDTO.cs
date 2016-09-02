ef872e0575b6079c5acbec5815671090ee2222cf
Vaibhav Bali <vibsbali@gmail.com>
2016-09-02 23:02:12 +1000

    Created a DTO

---
+++ Vidly/DTO/CustomerDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Vidly.Models;
+
+namespace Vidly.DTO
+{
+    public class CustomerDto
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter customer's name")]
+        [StringLength(255)]
+        public string Name { get; set; }
+
+        public bool IsSubscribedToNewsLetter { get; set; }
+
+        [Min18YearsIfAMember]
+        public DateTime? DateOfBirth { get; set; }
+
+        public int MembershipTypeId { get; set; }
+    }
+}
