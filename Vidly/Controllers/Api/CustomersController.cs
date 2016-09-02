072abb8c2aef67e62e4297407d9846637aa24df8
Vaibhav Bali <vibsbali@gmail.com>
2016-09-02 22:49:02 +1000

    Added a new WebApi Controller

---
+++ Vidly/Controllers/Api/CustomersController.cs
@@ -0,0 +1,87 @@
+﻿using System.Linq;
+using System.Web.Http;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext context;
+
+        public CustomersController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public IHttpActionResult GetCustomers()
+        {
+            var customers = context.Customers.ToList();
+            return Ok(customers);
+        }
+
+        public IHttpActionResult GetCustomer(int id)
+        {
+            var customer = context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(customer);
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateCustomer(Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            context.Customers.Add(customer);
+            context.SaveChanges();
+
+            return Created($"/api/customers/{customer.Id}", customer);
+        }
+
+        [HttpPost]
+        public IHttpActionResult UpdateCustomer(int id, Customer customer)
+        {
+            if (ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var customerFromDb = context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerFromDb == null)
+            {
+                return NotFound();
+            }
+
+            customerFromDb.Name = customer.Name;
+            customerFromDb.DateOfBirth = customer.DateOfBirth;
+            customerFromDb.MembershipTypeId = customer.MembershipTypeId;
+            customerFromDb.IsSubscribedToNewsLetter = customer.IsSubscribedToNewsLetter;
+
+            context.SaveChanges();
+            return Ok();
+        }
+
+        public IHttpActionResult DeleteCustomer(int id)
+        {
+            var customerFromDb = context.Customers.SingleOrDefault(c => c.Id == id);
+
+            if (customerFromDb == null)
+            {
+                return NotFound();
+            }
+
+            context.Customers.Remove(customerFromDb);
+            context.SaveChanges();
+
+            return StatusCode(System.Net.HttpStatusCode.NoContent);
+        }
+    }
+}
