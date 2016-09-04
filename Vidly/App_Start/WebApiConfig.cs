682f939d42f0facb03d680458602de0ecdc38bc4
Vaibhav Bali <vibsbali@gmail.com>
2016-09-04 11:22:42 +1000

    Added formatting settings in Config for Json configuration

--- Vidly/App_Start/WebApiConfig.cs
+++ Vidly/App_Start/WebApiConfig.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Newtonsoft.Json.Serialization;
 using System.Web.Http;
 
 namespace Vidly
@@ -10,6 +8,11 @@ namespace Vidly
         public static void Register(HttpConfiguration config)
         {
             config.MapHttpAttributeRoutes();
+
+            //created Json formatting
+            var settings = config.Formatters.JsonFormatter.SerializerSettings;
+            settings.ContractResolver = new CamelCasePropertyNamesContractResolver();
+            settings.Formatting = Newtonsoft.Json.Formatting.Indented;
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
